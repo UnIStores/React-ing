603effb21add9d33a950b70589a69d3a149d51e7
wkdrmadl3@naver.com <wkdrmadl3@naver.com>
2021-09-07 20:57:32 +0900

    테스트

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Counter from "./Counter";
+import Info from "./Info";
+import Average from "./Average";
+
+function App() {
+  return (
+    <div>
+      <Average />
+    </div>
+  );
+}
+
+export default App;
